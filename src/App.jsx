327a031ad9453d5996a807e801b6d4067f8c56ee
unknown <email.ahmadjehad@gmail.com>
2024-02-17 03:55:44 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Blog from "./pages/Blog/Blog";
+import AllBlogs from "./pages/AllBlogs/AllBlogs";
+import Nopage from "./pages/Nopage/Nopage";
+import BlogInfo from "./pages/BlogInfo/BlogInfo";
+import AdminLogin from "./pages/Admin/AdminLogin/AdminLogin";
+import Dashboard from "./pages/Admin/Dashboard/Dashboard";
+import MState from "./contexts/data/MState";
+import CreateBlog from "./pages/Admin/CreateBlog/CreateBlog";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <MState>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/allblogs" element={<AllBlogs />} />
+          <Route path="/bloginfo/:id" element={<BlogInfo />} />
+          <Route path="/adminlogin" element={<AdminLogin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/createblog" element={<CreateBlog />} />
+          <Route path="/*" element={<Nopage />} />
+        </Routes>
+        <Toaster/>
+      </Router>
+    </MState>
   )
 }
 
